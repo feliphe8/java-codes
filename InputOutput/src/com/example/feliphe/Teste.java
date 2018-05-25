c0448fb274a43038041e249446939ba488220561
Feliphe Simões Carvalho <fsc_nfsu@hotmail.com>
2018-05-25 19:39:02 -0300

    regular expression

---
+++ InputOutput/src/com/example/feliphe/Teste.java
@@ -0,0 +1,36 @@
+package com.example.feliphe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberLines = scanner.nextInt();
+        int a;
+        int b;
+        for (int i=0; i<numberLines;i++){
+            String input = scanner.nextLine();
+            String[] array = input.split(" ");
+            for (int j=0; j < array.length; j++){
+                int number = Integer.parseInt(array[i]);
+
+            }
+        }
+
+        System.out.println("Resultado: " + GreatestCommonDivisor(a, b));
+    }
+
+    public static int GreatestCommonDivisor(int a, int b){
+        if (a == 0)
+            return b;
+        while (b != 0) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        return a;
+    }
+}
